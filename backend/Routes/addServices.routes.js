7dec8df2f8a09b1bb666ff3d1952e7367d7b380c
Anisul Islam <anis.inbox10@gmail.com>
2024-11-17 01:42:39 +0500

    add services routes

--- backend/Routes/addServices.routes.js
+++ backend/Routes/addServices.routes.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { addService } from '../Controllers/addServices.controller.js';
+import { addService, deleteService, getAllServices, getServiceById, updateService } from '../Controllers/addServices.controller.js';
 import { verifyAdminToken } from '../Middleware/verifyToken.middleware.js';
 const router=express.Router();
 router.route("/post").post(verifyAdminToken,addService)
+router.route("/get").get(getAllServices)
+router.route("/:id").get(getServiceById)
+router.route("/update/:id").put(verifyAdminToken,updateService)
+router.route("/delete/:id").delete(verifyAdminToken,deleteService)
 
 export default router;
